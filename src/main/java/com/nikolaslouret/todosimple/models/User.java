98b92a18510c6eec47d3de27f75516c41d9f1abe
Nikolas Louret <navlouret@sga.pucminas.br>
2023-03-22 17:56:15 -0300

    [ADD] Criação do controller do user

--- src/main/java/com/nikolaslouret/todosimple/models/User.java
+++ src/main/java/com/nikolaslouret/todosimple/models/User.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -83,6 +84,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
